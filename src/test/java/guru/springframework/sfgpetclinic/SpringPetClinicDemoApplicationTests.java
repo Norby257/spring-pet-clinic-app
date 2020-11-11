daf61c46ce8e91509d1d45bc1b827cf17b78a980
nmadden3 <nmadden3@gatech.edu>
2020-11-11 09:34:50 -0600

    closes #5. Implement interfaces for services

--- src/test/java/guru/springframework/sfgpetclinic/SpringPetClinicDemoApplicationTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/SpringPetClinicDemoApplicationTests.java
@@ -1,6 +1,7 @@
-package com.demo.springpetclinicdemo;
+//package com.demo.springpetclinicdemo;
+package guru.springframework.sfgpetclinic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
